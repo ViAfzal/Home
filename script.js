fcf582959bec8d8a408e30346492a4de631ce9ad
Violet <98357871+ViAfzal@users.noreply.github.com>
2024-02-13 21:12:42 -0600

    Added navbar support via script

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+  $(".menu-toggle").click(() => {
+    $(".menu-toggle").toggleClass("menu-open");
+    $(".menu").toggleClass("active");
+  });
+  $(".link").click(() => {
+    $(".menu-toggle").toggleClass("menu-open");
+    $(".menu").toggleClass("active");
+  });
+});
